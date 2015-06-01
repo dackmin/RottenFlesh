d484be7ed3ec43a8a88e3bdc987b0d45611badba
Ugo Stephant <us@moonda.com>
2015-06-01 17:30:47 +0200

    remove tabs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,135 +1,135 @@
 module.exports = function(grunt) {
 
-	grunt.initConfig({
+    grunt.initConfig({
 
-		// Coffee build
-		coffee: {
-			build: {
-				options: {
-					join: true,
-					bare: true
-				},
-				files: {
-					'build/rotten-flesh.js': [
+        // Coffee build
+        coffee: {
+            build: {
+                options: {
+                    join: true,
+                    bare: true
+                },
+                files: {
+                    'build/rotten-flesh.js': [
 
-						// Main app file
-						'src/Main.coffee',
+                        // Main app file
+                        'src/Main.coffee',
 
-						// Utils
-						'src/utils/Utils.coffee',
-						'src/utils/Vendors.coffee',
-						'src/utils/EventManager.coffee',
-						'src/utils/InputManager.coffee',
+                        // Utils
+                        'src/utils/Utils.coffee',
+                        'src/utils/Vendors.coffee',
+                        'src/utils/EventManager.coffee',
+                        'src/utils/InputManager.coffee',
 
-						// Renders
-						'src/render/Engine.coffee',
-						'src/render/CanvasRender.coffee',
-						'src/render/WebGL2DRender.coffee',
+                        // Renders
+                        'src/render/Engine.coffee',
+                        'src/render/CanvasRender.coffee',
+                        'src/render/WebGL2DRender.coffee',
 
-						// Loaders
-						'src/loaders/ImageLoader.coffee',
-						'src/loaders/TiledMapLoader.coffee',
-						'src/loaders/AssetLoader.coffee',
+                        // Loaders
+                        'src/loaders/ImageLoader.coffee',
+                        'src/loaders/TiledMapLoader.coffee',
+                        'src/loaders/AssetLoader.coffee',
 
-						// Drawables
-						'src/drawable/Drawable.coffee',
-						'src/drawable/TiledMap.coffee',
-						'src/drawable/Sprite.coffee',
-						'src/drawable/SpriteSheet.coffee',
+                        // Drawables
+                        'src/drawable/Drawable.coffee',
+                        'src/drawable/TiledMap.coffee',
+                        'src/drawable/Sprite.coffee',
+                        'src/drawable/SpriteSheet.coffee',
 
-						// Game
-						'src/game/Game.coffee',
-						'src/game/Scene.coffee',
-					]
-				}
-			}
-		},
-
-
-		// Uglify stuff
-		uglify: {
-			options: {
-				mangle: false,
-			},
-			target: {
-				files: {
-					"build/rotten-flesh.min.js": "build/rotten-flesh.js"
-				}
-			}
-		},
+                        // Game
+                        'src/game/Game.coffee',
+                        'src/game/Scene.coffee',
+                    ]
+                }
+            }
+        },
 
 
-		// Watch task
-		watch: {
-			coffeeScripts: {
-				files: "src/**/*.coffee",
-				tasks: "coffee:build",
-				options: { livereload: '<%= connect.options.livereload %>' }
-			},
-
-			gruntfile: {
-				files: ['Gruntfile.js'],
-				tasks: "coffee:build"
-			},
-
-			options: {
-				livereload: '<%= connect.options.livereload %>',
-				files: ["build/*.js","examples/*/*.html"]
-			}
-		},
+        // Uglify stuff
+        uglify: {
+            options: {
+                mangle: false,
+            },
+            target: {
+                files: {
+                    "build/rotten-flesh.min.js": "build/rotten-flesh.js"
+                }
+            }
+        },
 
 
-		//Serve task
-		connect: {
-			options: {
-				port: 35565,
-				hostname: "localhost",
-				livereload: 35566
-			},
+        // Watch task
+        watch: {
+            coffeeScripts: {
+                files: "src/**/*.coffee",
+                tasks: "coffee:build",
+                options: { livereload: '<%= connect.options.livereload %>' }
+            },
 
-			livereload: {
-				options: {
-					open: true,
-					middleware: function (connect) {
-						return [
-							connect.static('examples'),
-							connect().use('/build', connect.static('./build'))
-						];
-					}
-				}
-			}
-		},
-	});
+            gruntfile: {
+                files: ['Gruntfile.js'],
+                tasks: "coffee:build"
+            },
 
-	grunt.loadNpmTasks('grunt-contrib-coffee');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-connect');
+            options: {
+                livereload: '<%= connect.options.livereload %>',
+                files: ["build/*.js","examples/*/*.html"]
+            }
+        },
 
-	grunt.registerTask("build", [
-		"coffee:build"
-	]);
 
-	grunt.registerTask("dist", [
-		"coffee:build",
-		"uglify"
-	]);
+        //Serve task
+        connect: {
+            options: {
+                port: 35565,
+                hostname: "localhost",
+                livereload: 35566
+            },
 
-	grunt.registerTask("listen", [
-		"coffee:build",
-		"watch"
-	]);
+            livereload: {
+                options: {
+                    open: true,
+                    middleware: function (connect) {
+                        return [
+                            connect.static('examples'),
+                            connect().use('/build', connect.static('./build'))
+                        ];
+                    }
+                }
+            }
+        },
+    });
 
-	grunt.registerTask("serve", "", function(){
-		grunt.task.run([
-			"coffee:build",
-			"connect:livereload",
-			"watch"
-		]);
-	});
+    grunt.loadNpmTasks('grunt-contrib-coffee');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
-	grunt.registerTask("test", [
+    grunt.registerTask("build", [
+        "coffee:build"
+    ]);
 
-	]);
+    grunt.registerTask("dist", [
+        "coffee:build",
+        "uglify"
+    ]);
+
+    grunt.registerTask("listen", [
+        "coffee:build",
+        "watch"
+    ]);
+
+    grunt.registerTask("serve", "", function(){
+        grunt.task.run([
+            "coffee:build",
+            "connect:livereload",
+            "watch"
+        ]);
+    });
+
+    grunt.registerTask("test", [
+
+    ]);
 
 };
